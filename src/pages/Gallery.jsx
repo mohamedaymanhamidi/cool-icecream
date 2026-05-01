d8afc7c1841e05f20e568905d57641cd54120ea6
Ayman <mac@Aymans-Macbook-pro.local>
2026-05-01 23:45:49 +0100

    update

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,6 +1,7 @@
-import img1 from "../assets/ice1.jpg";
-import img2 from "../assets/ice2.jpg";
-import img3 from "../assets/ice3.jpg";
+import img1 from "../assets/img1.jpeg";
+import img2 from "../assets/img2.jpeg";
+import img3 from "../assets/img3.jpeg";
+import img4 from "../assets/img4.jpeg";
 
 export default function Gallery() {
   return (
@@ -9,7 +10,7 @@ export default function Gallery() {
 
       <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))", gap: "20px" }}>
         
-        {[img1, img2, img3].map((img, i) => (
+        {[img1, img2, img3, img4].map((img, i) => (
           <div className="card" key={i}>
             <img 
               src={img} 
